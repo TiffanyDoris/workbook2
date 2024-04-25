014b230b0245699ea08503fb3522f178a22a98e0
tiffanydoris <tiffanydoris12@gmail.com>
2024-04-25 14:52:52 -0400

    show_major.js

--- exercises/show_major.js
+++ exercises/show_major.js
@@ -1,14 +1,14 @@
-"use strict"
+"use strict";
 
-var studentName = "Tiffy Dor"
-var studentMajor = "CSCI"
+var studentName = "Tiffy Dor";
+var studentMajor = "CSCI";
 
-if (studentMajor =="csci") {
-    majorName ="computer science"
-} else if (studentMajor =="biol") {
-    majorName ="biology"
-}else if (studentMajor =="mkt") {
-    majorName ="marketing"
+if (studentMajor == "csci") {
+  majorName = "computer science";
+} else if (studentMajor == "biol") {
+  majorName = "biology";
+} else if (studentMajor == "mkt") {
+  majorName = "marketing";
 }
 
-console.login(studentName + " is majoring in")
+console.login(studentName + " is majoring in");
