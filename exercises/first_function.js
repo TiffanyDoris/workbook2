3cbe2ba44c84153b7a937464da6fb5c691af6655
tiffanydoris <tiffanydoris12@gmail.com>
2024-04-25 16:03:52 -0400

    first_f.js

---
+++ exercises/first_function.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function greet() {
+  let message = "Hello World!";
+  console.log(message);
+}
+
+function brushTeeth() {
+  console.log("brushed done");
+}
+
+greet();
+brushTeeth();
